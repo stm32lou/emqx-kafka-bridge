cd7ed0d1b6f511a0fb46b7ae1bd53af8c4b55324
louis <370959225@qq.com>
2020-05-28 13:33:53 +0800

    shield printf

--- src/emqx_kafka_bridge.erl
+++ src/emqx_kafka_bridge.erl
@@ -77,13 +77,13 @@ load(Env) ->
 %%--------------------------------------------------------------------
 
 on_client_connect(ConnInfo = #{clientid := ClientId}, Props, _Env) ->
-  io:format("Client(~s) connect, ConnInfo: ~p, Props: ~p~n",
-    [ClientId, ConnInfo, Props]),
+  %%io:format("Client(~s) connect, ConnInfo: ~p, Props: ~p~n",
+  %%  [ClientId, ConnInfo, Props]),
   {ok, Props}.
 
 on_client_connack(ConnInfo = #{clientid := ClientId}, Rc, Props, _Env) ->
-  io:format("Client(~s) connack, ConnInfo: ~p, Rc: ~p, Props: ~p~n",
-    [ClientId, ConnInfo, Rc, Props]),
+  % io:format("Client(~s) connack, ConnInfo: ~p, Rc: ~p, Props: ~p~n",
+  %   [ClientId, ConnInfo, Rc, Props]),
   {ok, Props}.
 
 % on_client_connected(ClientInfo = #{clientid := ClientId}, ConnInfo, _Env) ->
@@ -91,8 +91,8 @@ on_client_connack(ConnInfo = #{clientid := ClientId}, Rc, Props, _Env) ->
 %     [ClientId, ClientInfo, ConnInfo]);
 
 on_client_connected(ClientInfo = #{clientid := ClientId, username := Username, peerhost := Peerhost}, ConnInfo, _Env) ->
-    io:format("Client(~s) connected, ClientInfo:~n~p~n, ConnInfo:~n~p~n",
-      [ClientId, ClientInfo, ConnInfo]), 
+    % io:format("Client(~s) connected, ClientInfo:~n~p~n, ConnInfo:~n~p~n",
+    %   [ClientId, ClientInfo, ConnInfo]), 
     Params = #{ action => connected,
             clientid => ClientId,
             username => Username,
@@ -115,8 +115,8 @@ on_client_connected(#{}, _ConnInfo, _Env) ->
 
 
 on_client_disconnected(ClientInfo = #{clientid := ClientId}, {shutdown, Reason}, ConnInfo, Env) when is_atom(Reason) ->
-    io:format("Client(~s) disconnected due to ~p, ClientInfo:~n~p~n, ConnInfo:~n~p~n",
-      [ClientId, Reason, ClientInfo, ConnInfo]),
+    % io:format("Client(~s) disconnected due to ~p, ClientInfo:~n~p~n, ConnInfo:~n~p~n",
+    %   [ClientId, Reason, ClientInfo, ConnInfo]),
     on_client_disconnected(ClientInfo, Reason, ConnInfo, Env);
 
 on_client_disconnected(#{clientid := ClientId, username := Username}, Reason, ConnInfo, _Env) ->
@@ -136,47 +136,68 @@ printable(Term) when is_tuple(Term) ->
     iolist_to_binary(io_lib:format("~p", [Term])).
 
 on_client_authenticate(_ClientInfo = #{clientid := ClientId}, Result, _Env) ->
-  io:format("Client(~s) authenticate, Result:~n~p~n", [ClientId, Result]),
+  % io:format("Client(~s) authenticate, Result:~n~p~n", [ClientId, Result]),
   {ok, Result}.
 
 on_client_check_acl(_ClientInfo = #{clientid := ClientId}, Topic, PubSub, Result, _Env) ->
-  io:format("Client(~s) check_acl, PubSub:~p, Topic:~p, Result:~p~n",
-    [ClientId, PubSub, Topic, Result]),
+  % io:format("Client(~s) check_acl, PubSub:~p, Topic:~p, Result:~p~n",
+  %   [ClientId, PubSub, Topic, Result]),
   {ok, Result}.
 
 on_client_subscribe(#{clientid := ClientId}, _Properties, TopicFilters, _Env) ->
-  io:format("Client(~s) will subscribe: ~p~n", [ClientId, TopicFilters]),
+  % io:format("Client(~s) will subscribe: ~p~n", [ClientId, TopicFilters]),
   {ok, TopicFilters}.
 
 on_client_unsubscribe(#{clientid := ClientId}, _Properties, TopicFilters, _Env) ->
-  io:format("Client(~s) will unsubscribe ~p~n", [ClientId, TopicFilters]),
+  % io:format("Client(~s) will unsubscribe ~p~n", [ClientId, TopicFilters]),
   {ok, TopicFilters}.
 
 %%--------------------------------------------------------------------
 %% Session Lifecircle Hooks
 %%--------------------------------------------------------------------
 
+% on_session_created(#{clientid := ClientId}, SessInfo, _Env) ->
+%   io:format("Session(~s) created, Session Info:~n~p~n", [ClientId, SessInfo]).
+
+% on_session_subscribed(#{clientid := ClientId}, Topic, SubOpts, _Env) ->
+%   io:format("Session(~s) subscribed ~s with subopts: ~p~n", [ClientId, Topic, SubOpts]).
+
+% on_session_unsubscribed(#{clientid := ClientId}, Topic, Opts, _Env) ->
+%   io:format("Session(~s) unsubscribed ~s with opts: ~p~n", [ClientId, Topic, Opts]).
+
+% on_session_resumed(#{clientid := ClientId}, SessInfo, _Env) ->
+%   io:format("Session(~s) resumed, Session Info:~n~p~n", [ClientId, SessInfo]).
+
+% on_session_discarded(_ClientInfo = #{clientid := ClientId}, SessInfo, _Env) ->
+%   io:format("Session(~s) is discarded. Session Info: ~p~n", [ClientId, SessInfo]).
+
+% on_session_takeovered(_ClientInfo = #{clientid := ClientId}, SessInfo, _Env) ->
+%   io:format("Session(~s) is takeovered. Session Info: ~p~n", [ClientId, SessInfo]).
+
+% on_session_terminated(_ClientInfo = #{clientid := ClientId}, Reason, SessInfo, _Env) ->
+%   io:format("Session(~s) is terminated due to ~p~nSession Info: ~p~n",
+%     [ClientId, Reason, SessInfo]).
+
 on_session_created(#{clientid := ClientId}, SessInfo, _Env) ->
-  io:format("Session(~s) created, Session Info:~n~p~n", [ClientId, SessInfo]).
+  ok.
 
 on_session_subscribed(#{clientid := ClientId}, Topic, SubOpts, _Env) ->
-  io:format("Session(~s) subscribed ~s with subopts: ~p~n", [ClientId, Topic, SubOpts]).
+  ok.
 
 on_session_unsubscribed(#{clientid := ClientId}, Topic, Opts, _Env) ->
-  io:format("Session(~s) unsubscribed ~s with opts: ~p~n", [ClientId, Topic, Opts]).
+  ok.
 
 on_session_resumed(#{clientid := ClientId}, SessInfo, _Env) ->
-  io:format("Session(~s) resumed, Session Info:~n~p~n", [ClientId, SessInfo]).
+  ok.
 
 on_session_discarded(_ClientInfo = #{clientid := ClientId}, SessInfo, _Env) ->
-  io:format("Session(~s) is discarded. Session Info: ~p~n", [ClientId, SessInfo]).
+  ok.
 
 on_session_takeovered(_ClientInfo = #{clientid := ClientId}, SessInfo, _Env) ->
-  io:format("Session(~s) is takeovered. Session Info: ~p~n", [ClientId, SessInfo]).
+  ok.
 
 on_session_terminated(_ClientInfo = #{clientid := ClientId}, Reason, SessInfo, _Env) ->
-  io:format("Session(~s) is terminated due to ~p~nSession Info: ~p~n",
-    [ClientId, Reason, SessInfo]).
+  ok.
 
 %%--------------------------------------------------------------------
 %% Message PubSub Hooks
@@ -227,17 +248,19 @@ on_message_publish(Message = #message{topic = Topic, flags = #{retain := Retain}
 on_message_dropped(#message{topic = <<"$SYS/", _/binary>>}, _By, _Reason, _Env) ->
   ok;
 on_message_dropped(Message, _By = #{node := Node}, Reason, _Env) ->
-  io:format("Message dropped by node ~s due to ~s: ~s~n",
-    [Node, Reason, emqx_message:format(Message)]).
+  % io:format("Message dropped by node ~s due to ~s: ~s~n",
+  % [Node, Reason, emqx_message:format(Message)]).
+  ok.
 
 on_message_delivered(_ClientInfo = #{clientid := ClientId}, Message, _Env) ->
-  io:format("Message delivered to client(~s): ~s~n",
-    [ClientId, emqx_message:format(Message)]),
+  % io:format("Message delivered to client(~s): ~s~n",
+  % [ClientId, emqx_message:format(Message)]),
   {ok, Message}.
 
 on_message_acked(_ClientInfo = #{clientid := ClientId}, Message, _Env) ->
-  io:format("Message acked by client(~s): ~s~n",
-    [ClientId, emqx_message:format(Message)]).
+  % io:format("Message acked by client(~s): ~s~n",
+  % [ClientId, emqx_message:format(Message)]).
+  ok.
 
 %% Called when the plugin application stop
 unload() ->
